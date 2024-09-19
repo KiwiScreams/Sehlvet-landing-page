f8858c3c524be86856f33d0969fefcee1b8e421e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 21:04:22 +0400

    mobile nav

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -43,6 +43,44 @@ const Header = () => {
           </div>
         </div>
       </header>
+      <header className="mobile">
+        <h1>
+          <NavLink to="/">Sehlvet</NavLink>
+        </h1>
+        <nav>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/collections"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Collections
+          </NavLink>
+          <NavLink
+            to="/brands"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Brands
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            About Us
+          </NavLink>
+        </nav>
+        <div className="icons">
+          <img src={bookmarkIcon} alt="" />
+          <div className="cart">
+            <img src={bagIcon} alt="" />
+            <div className="quantity">0</div>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
