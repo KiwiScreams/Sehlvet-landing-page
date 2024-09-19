d92410990dc1994db0e04d66dd037caed7ff0447
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 19:55:50 +0400

    not found page

--- src/pages/not found/NotFound.jsx
+++ src/pages/not found/NotFound.jsx
@@ -1,8 +1,19 @@
 import "./NotFound.css";
+import errorImg from "../../assets/images/images/404.png";
 const NotFound = () => {
-    return ( 
-        <>not found page</>
-     );
-}
- 
+  return (
+    <>
+      <section className="not-found--page">
+        <div className="error-image">
+            <img src={errorImg} alt="" />
+        </div>
+        <div className="error-text">
+            <h2>Oops</h2>
+            <p>...</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default NotFound;
