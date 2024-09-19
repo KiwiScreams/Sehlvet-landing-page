0ced1bbb6c88b708d9b1cabe19f87fe381ee05a7
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 23:14:03 +0400

    box

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -2,6 +2,8 @@ import "./Trending.css";
 import img1 from "../../assets/images/images/nikita-kachanovsky-GY6ViMxtmDE-unsplash.png";
 import img2 from "../../assets/images/images/nathan-dumlao-aT2Voo0gyVE-unsplash.png";
 import img3 from "../../assets/images/images/nikita-kachanovsky-GY6ViMxtmDE-unsplash (1).png";
+import bookmarkIcon from "../../assets/images/icons/Bookmark.svg";
+import bagIcon from "../../assets/images/icons/Bag.svg";
 const Trending = () => {
   return (
     <>
@@ -11,6 +13,21 @@ const Trending = () => {
             <span>Trending</span> Collections
           </h2>
           <p>Have a look on what's trending now!</p>
+        </div>
+        <div className="images">
+          <div className="box">
+            <div className="image-container">
+              <img src={img1} alt="" />
+            </div>
+            <h4>Formal Shirt For Woman</h4>
+            <div className="box-bottom">
+              <p>Price: $25.38</p>
+              <div className="flex-box">
+                <img src={bookmarkIcon} alt="" />
+                <img src={bagIcon} alt="" />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
