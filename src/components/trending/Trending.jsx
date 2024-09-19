80bc54c0f44dc00fdf9c27d5e1b79abd43d6d236
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 23:02:31 +0400

    trending header styles

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -1,8 +1,17 @@
 import "./Trending.css";
 const Trending = () => {
-    return ( 
-        <>tr</>
-     );
-}
- 
+  return (
+    <>
+      <section className="trending--component">
+        <div className="trending-header">
+          <h2>
+            <span>Trending</span> Collections
+          </h2>
+          <p>Have a look on what's trending now!</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Trending;
