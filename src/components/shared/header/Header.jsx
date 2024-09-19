c66277bb9708a400fcb071511691bc4c550ed43b
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 21:16:35 +0400

    delete import

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 import bookmarkIcon from "../../../assets/images/icons/Bookmark.svg";
 import bagIcon from "../../../assets/images/icons/Bag.svg";
