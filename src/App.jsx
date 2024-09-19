1eb1420ad1bb8d5eb9af0e2c1b583ae1ef30bcef
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 19:50:30 +0400

    not found routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import NotFound from "./pages/not found/NotFound";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
   );
