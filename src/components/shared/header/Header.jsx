807e7c547660e0c78a7e64f3c123cdbb0e03cc7d
KiwiScreams <screamskiwi3@gmail.com>
2024-09-19 20:37:47 +0400

    header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
-    return ( 
-        <>header</>
-     );
-}
- 
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to="/">Sehlvet</Link>
+        </h1>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/collections">Collections</Link>
+            <Link to="/brands">Brands</Link>
+            <Link to="/about">About Us</Link>
+        </nav>
+      </header>
+    </>
+  );
+};
+
 export default Header;
