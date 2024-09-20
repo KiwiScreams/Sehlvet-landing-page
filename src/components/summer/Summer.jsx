fa3a10b552d4199d0b58d56ae7f2907475f699f3
KiwiScreams <screamskiwi3@gmail.com>
2024-09-20 19:37:09 +0400

    summer component

--- src/components/summer/Summer.jsx
+++ src/components/summer/Summer.jsx
@@ -1,4 +1,5 @@
 import "./Summer.css";
+import img1 from "../../assets/images/images/Rectangle 14.png";
 const Summer = () => {
   return (
     <>
@@ -8,6 +9,28 @@ const Summer = () => {
             <span>Trending</span> Collections
           </h2>
           <p>Have a look on what's trending now!</p>
+        </div>
+        <div className="summer-body">
+          <div className="left-container">
+            <img src={img1} alt="" />
+            <p>
+              Our main aim is to serve our customer with better quality product.
+              We try to understand their needs and provide them within a short
+              period of time.
+            </p>
+          </div>
+          <div className="left-container">
+            <p>
+              Our main aim is to serve our customer with better quality product.
+              We try to understand their needs and provide them within a short
+              period of time.
+            </p>
+            <div className="img-container">
+              <img src={img1} alt="" />
+              <img src={img1} alt="" />
+            </div>
+            <button>Explore</button>
+          </div>
         </div>
       </section>
     </>
