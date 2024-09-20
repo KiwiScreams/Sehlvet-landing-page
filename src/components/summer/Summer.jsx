45d789f30b38aca39b62e86338846de068a2260d
KiwiScreams <screamskiwi3@gmail.com>
2024-09-20 19:22:01 +0400

    summer header

--- src/components/summer/Summer.jsx
+++ src/components/summer/Summer.jsx
@@ -1,8 +1,17 @@
 import "./Summer.css";
 const Summer = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  return (
+    <>
+      <section className="summer--component">
+        <div className="summer-header">
+          <h2>
+            <span>Trending</span> Collections
+          </h2>
+          <p>Have a look on what's trending now!</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Summer;
