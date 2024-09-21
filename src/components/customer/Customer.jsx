c09f47d058c189cbf39a28b81a7e8da50a01dfea
KiwiScreams <screamskiwi3@gmail.com>
2024-09-21 12:39:16 +0400

    custumer header

--- src/components/customer/Customer.jsx
+++ src/components/customer/Customer.jsx
@@ -1,12 +1,16 @@
 import "./Customer.css";
 const Customer = () => {
-    return ( 
-        <>
-        <section className="custumer--component">
-            l
-        </section>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="custumer--component">
+        <div className="customer-header">
+          <h2>
+            What our <span>Customer Says</span>
+          </h2>
+          <p>We value our customer's feedback to provide the best service.</p>
+        </div>
+      </section>
+    </>
+  );
+};
 export default Customer;
