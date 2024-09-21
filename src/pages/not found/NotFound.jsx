42d090dd66ea1d348ee7e8749f133e044a7d90d1
KiwiScreams <screamskiwi3@gmail.com>
2024-09-21 23:43:41 +0400

    current page name in undefined page

--- src/pages/not found/NotFound.jsx
+++ src/pages/not found/NotFound.jsx
@@ -1,6 +1,12 @@
 import "./NotFound.css";
 import errorImg from "../../assets/images/images/404.png";
+import { useLocation } from "react-router-dom";
 const NotFound = () => {
+  const pageNames = {
+    "/about": "About Us",
+  };
+  const location = useLocation();
+  const currentPageName = pageNames[location.pathname] || "Unknown Page";
   return (
     <>
       <section className="not-found--page">
@@ -11,8 +17,9 @@ const NotFound = () => {
           <div className="error-text">
             <h2>Oops...</h2>
             <p>
-              We couldn't find that page. Please check the URL or return to the
-              homepage.
+              We couldn't find the page you're looking for: 
+               {currentPageName}. Please check the URL or return
+              to the homepage.
             </p>
           </div>
         </div>
